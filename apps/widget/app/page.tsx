b9236c1ce82c165c44991c7e57ce592a31402bc1
Shubh Jain <shubhjain191@gmail.com>
2025-12-09 14:42:11 +0530

    initial setup and added monorepo & turborepo with shadcn

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
+        <h1 className="text-2xl font-bold">Hello World/widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
       </div>
     </div>
   )
